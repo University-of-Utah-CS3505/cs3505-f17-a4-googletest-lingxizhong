f77f88b2d2764a1361b6a16f291b68220d6c34cd
LINGXI-XPS\Lingxi <lingxizhong@gmail.com>
2017-09-28 14:54:34 -0600

    Fixed main to return 0

--- StudentsTests.cpp
+++ StudentsTests.cpp
@@ -12,7 +12,8 @@ using namespace std;
 int main(int argc, char **argv) {
   printf("Running main() from gtest_main.cc\n");
   testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  RUN_ALL_TESTS();
+  return 0;
 }
 
 
